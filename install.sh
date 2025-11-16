3e8ddb388774dbe45fa4ac5e91a8f90754c1d577
yxKryptonite <1535820963@qq.com>
2025-11-16 00:00:02 -0500

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 conda create -n robopose python=3.9
 conda activate robopose
 
-pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118 # this is my pytorch version, please install pytorch based on your cuda version
 
-pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable"
+# install pytorch3d, this may take a while
+# please contact Yuxuan if you encounter any issue here
+pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable" --no-build-isolation
 
 pip install -r requirements.txt
